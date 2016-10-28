72e166b2b5a0a299b445ea6471ecad6c92a49122
Samuel Villavicencio <savillavicencio@csumb.edu>
2016-10-27 17:39:05 -0700

    Fix MovieRepositoryTest by adding test profile.
    Summary: Use embedded neo4j driver for testing
    
    Test Plan: Verify CircleCI builds with current configuration
    
    Reviewers: hargueta, clarissavazquez, chmartinez1
    
    Reviewed By: chmartinez1
    
    Subscribers: #movieme
    
    Differential Revision: http://csumbse.net/D110

--- server/src/test/java/com/movie/me/repository/MovieRepositoryTest.java
+++ server/src/test/java/com/movie/me/repository/MovieRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ActiveProfiles;
 
 
 import static org.junit.Assert.*;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest()
+@ActiveProfiles(profiles = "test")
 public class MovieRepositoryTest {
     @Autowired
     MovieRepository movieRepository;
