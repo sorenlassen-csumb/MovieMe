11c521fd4605c41f5b76d36fa2ded56c63184f64
Samuel Villavicencio <savillavicencio@csumb.edu>
2016-11-04 16:34:15 -0700

    Set up code coverage for server java code. Add unit tests for MovieRepository findByImdbId, findByTitleLike, & countLikesOf methods.

--- server/src/main/java/com/movie/me/repository/MovieRepository.java
+++ server/src/main/java/com/movie/me/repository/MovieRepository.java
@@ -8,8 +8,17 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MovieRepository extends GraphRepository<Movie> {
+    @Query("MATCH (m:MOVIE {IMDBID:{imdbid}}) " +
+            "RETURN m")
+    Movie findByImdbId(@Param("imdbid") String imdbid);
+    
     @Query("MATCH (m:MOVIE) " +
             "WHERE m.TITLE =~ ('(?i).*'+{title}+'.*')" +
             "RETURN m")
     List<Movie> findByTitleLike(@Param("title") String title);
+    
+    @Query("MATCH(m:MOVIE {IMDBID:{imdbid}})" +
+            "<-[l:LIKES]-() " + 
+            "RETURN COUNT(l)")
+    int countLikesOf(@Param("imdbid") String imdbid);
 }
--- server/src/test/java/com/movie/me/repository/MovieRepositoryTest.java
+++ server/src/test/java/com/movie/me/repository/MovieRepositoryTest.java
@@ -1,18 +1,23 @@
 package com.movie.me.repository;
 
 import com.movie.me.domain.Movie;
+import com.movie.me.domain.User;
+
 import org.junit.Test;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.*;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,27 +26,90 @@ import java.util.List;
 public class MovieRepositoryTest {
     @Autowired
     MovieRepository movieRepository;
+    
+    @Autowired
+    UserRepository userRepository;
+    
+    private Movie newHope;
+    private Movie empireStrikesBack;
+    private Movie returnOfTheJedi;
 
+    @Before
+    public void initialize() {
+        newHope = new Movie();
+        newHope.setTitle("Star Wars: Episode IV - A New Hope");
+        newHope.setImdbid("0004");
+        
+        empireStrikesBack = new Movie();
+        empireStrikesBack.setTitle("Star Wars: Episode V - Empire Strikes Back");
+        empireStrikesBack.setImdbid("0008");
+        
+        returnOfTheJedi = new Movie();
+        returnOfTheJedi.setTitle("Star Wars: Episode VI - Return of the Jedi");
+        returnOfTheJedi.setImdbid("0016");
+        
+        movieRepository.save(Arrays.asList(newHope, empireStrikesBack, returnOfTheJedi));
+    }
+    
     @Test
-    public void testEmptyResult() {
-        List<Movie> result = movieRepository.findByTitleLike("abcdefg");
+    @DirtiesContext
+    public void testFindByImdbIdSuccessfulResult() {
+        Movie result = movieRepository.findByImdbId("0004");
+        
+        assertThat(result.equals(newHope), is(true));
+    }
+    
+    @Test
+    @DirtiesContext
+    public void testFindByImdbIdEmptyResult() {
+        Movie result = movieRepository.findByImdbId("0001");
+        
+        assertThat(result, is(nullValue()));
+    }
+    
+    @Test
+    @DirtiesContext
+    public void testFindByTitleLikeSuccessfulResult() {
+        List<Movie> result = movieRepository.findByTitleLike("star");
 
-        assertThat(result.size(), equalTo(0));
+        assertThat(result.size(), equalTo(3));
+        assertThat(result, containsInAnyOrder(newHope, empireStrikesBack, returnOfTheJedi));
+    }
+    
+    @Test
+    @DirtiesContext
+    public void testFindByTitleLikeEmptyResult() {
+        List<Movie> result = movieRepository.findByTitleLike("The Phantom Menace");
+
+        assertThat(result.isEmpty(), is(true));
     }
 
     @Test
-    public void testCaseSensitiveSearch() {
+    @DirtiesContext
+    public void testFindByTitleLikeCaseInsensitive() {
         List<Movie> lowerCase = movieRepository.findByTitleLike("star");
         List<Movie> upperCase = movieRepository.findByTitleLike("STAR");
 
         assertThat(lowerCase, equalTo(upperCase));
     }
-
-    @Ignore
+    
     @Test
-    public void testSuccessfulResult() {
-        List<Movie> result = movieRepository.findByTitleLike("star");
-
-        assertTrue(result.size() > 0);
+    @DirtiesContext
+    public void testCountLikesOfMovieIsZero() {
+        int count = movieRepository.countLikesOf("0004");
+        
+        assertThat(count, equalTo(0));
+    }
+    
+    @Test
+    @DirtiesContext
+    public void testCountLikesOfMovieIsMoreThanZero() {
+        User samuel = new User();
+        samuel.setMoviesLiked(new HashSet<Movie>(Arrays.asList(newHope)));
+        userRepository.save(samuel);
+        
+        int count = movieRepository.countLikesOf("0004");
+        
+        assertThat(count, equalTo(1));
     }
 }
