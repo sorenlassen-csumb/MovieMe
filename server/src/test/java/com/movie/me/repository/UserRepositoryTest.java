e611c9adf80c64f3e4eb8b5fbb8570a6b6f4e508
chmartinez1 <chmartinez+test@csumb.edu>
2016-11-07 19:52:39 -0800

    Wrote tests for user Repository. Tested User liking a movie and user retrieving their previous likes
    Summary: Wrote tests to see what would happen if a user that was not in our system wanted to see their likes and what would happen when a user likes a movie
    
    Test Plan: I wrote tests so make sure that they work
    
    Reviewers: clarissavazquez, hargueta, samuelrey
    
    Reviewed By: samuelrey
    
    Differential Revision: http://csumbse.net/D217

--- server/src/test/java/com/movie/me/repository/UserRepositoryTest.java
+++ server/src/test/java/com/movie/me/repository/UserRepositoryTest.java
@@ -1,9 +1,11 @@
 package com.movie.me.repository;
 
 import java.util.List;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import org.junit.runner.RunWith;
@@ -24,13 +26,32 @@ import com.movie.me.beans.Neo4jTestConfiguration;
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    MovieRepository movieRepository;
 
+    private User samuel;
+    private Movie newHope;
     @Before
     public void initialize() {
+        samuel = new User();
+        samuel.setUserId("sammy123");
+        samuel.setName("Samuel Villavicencio");
+        samuel.setEmail("savillavicencio@csumb.edu");
+        
+        newHope = new Movie();
+        newHope.setTitle("Star Wars: Episode IV - A New Hope");
+        newHope.setImdbid("0004");
+        
+        userRepository.save(samuel);
+        movieRepository.save(Arrays.asList(newHope));
+        
+        
         return;
     }
 
     @Test
+    @DirtiesContext
     public void testCaseInsensitiveSearch() {
         List<User> lowerCase = userRepository.findByNameLike("samuel");
         List<User> upperCase = userRepository.findByNameLike("SAMUEL");
@@ -39,7 +60,33 @@ public class UserRepositoryTest {
     }
 
     @Test
+    @DirtiesContext
     public void testLikeNoDuplicates() {
         return;
     }
+    
+    @Test
+    @DirtiesContext
+    public void testRetrieveMoviesLikedByNonexistentUser(){
+        List<Movie> result = userRepository.retrieveMoviesLikedBy("Pearce");
+        assertThat(result.isEmpty(),is(true));
+    }
+    
+    @Test
+    @DirtiesContext
+    public void testaddUserLikesMovieNonexistentUser(){
+        Movie result = userRepository.addUserLikesMovie("Pearce","0004");
+        assertThat(result, equalTo(null));
+    }
+    
+    @Test
+    @DirtiesContext
+    public void testaddUserLikesMovieExistentUser(){
+        Movie result = userRepository.addUserLikesMovie("sammy123","0004");
+        assertThat(result, equalTo(newHope));
+    }
+
+
+    
+    
 }
