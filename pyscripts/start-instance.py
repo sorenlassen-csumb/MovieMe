e74489ed0485910a18bb9bd18742bb36c0c957e6
clarissavazquez <cvasquez-ramo@csumb.edu>
2016-11-20 17:48:26 -0800

    Automated action to stop an instance.
    Summary: Modified start-instance so that it includes the method to stop an instance as well.
    
    Test Plan: python [start/stop] [instance_id]
    
    Reviewers: chmartinez1, hargueta, samuelrey
    
    Subscribers: #movieme

--- pyscripts/start-instance.py
+++ pyscripts/start-instance.py
@@ -2,24 +2,43 @@ import boto3
 from botocore.exceptions import ClientError
 import sys
 
-client = boto3.client('ec2')
+client = boto3.client("ec2")
 
 def get_public_dns(instance):
-    return instance['Reservations'][0]['Instances'][0]['PublicDnsName']
+    return instance["Reservations"][0]["Instances"][0]["PublicDnsName"]
 
-def start(instance_id):
+def start_instance(instance_id):
     try:
         instance = client.start_instances(
             InstanceIds=[
                 instance_id,
             ]
         )
+        # TODO: SSH tunnel to instance and start neo4j service [11/20/16, Clarissa Vazquez]
+    except ClientError as e:
+        print e
+
+def stop_instance(instance_id):
+    try:
+        instance = client.stop_instances(
+            InstanceIds=[
+                instance_id,
+            ]
+        )
+        #TODO: SSH tunnel to instance and stop neo4j service [11/20/16, Clarissa Vazquez]
     except ClientError as e:
         print e
             
 
 if __name__ == "__main__":
-    if(len(sys.argv) > 1):
-        start(sys.argv[1])
+    # First argument should be either "start" or "stop".
+    # Second argument should be a valid EC2 instance id.
+    if len(sys.argv) == 3:
+        if sys.argv[1] == "start":
+            start_instance(sys.argv[2])
+        elif sys.argv[1] == "stop":
+            stop_instance(sys.argv[2])
+        else:
+            print "Invalid argument."
     else: 
-        print "Incorrect amount of arguments received."
+        print "Incorrect arguments."
